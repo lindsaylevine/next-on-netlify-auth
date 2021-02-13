1b4addcf7a401a07b7202bda9150b4a17cad316c
Zahid <zahid@zmdev.com>
2021-02-13 21:54:24 +0000

    append NEXTAUTH_URL with public

--- constants/config.js
+++ constants/config.js
@@ -2,5 +2,5 @@ export default {
     emailServerAddress: process.env.EMAIL_SERVER_ADDRESS,
     emailFrom: process.env.EMAIL_FROM,
     identityDbUrl: process.env.IDENTITY_DB_URL,
-    nextAuthUrl: process.env.NEXTAUTH_URL
+    nextAuthUrl: process.env.NEXT_PUBLIC_NEXTAUTH_URL
 }
