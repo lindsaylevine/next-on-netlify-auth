51f04f16bc95c477be126e11b89f01f0eee2ffa2
Zahid <zahid@zmdev.com>
2021-02-13 23:10:01 +0000

    updated NEXTAUTH_URL and removed it from the config since it's not used elsehwere in the app

--- constants/config.js
+++ constants/config.js
@@ -1,6 +1,5 @@
 export default {
     emailServerAddress: process.env.EMAIL_SERVER_ADDRESS,
     emailFrom: process.env.EMAIL_FROM,
-    identityDbUrl: process.env.IDENTITY_DB_URL,
-    nextAuthUrl: process.env.NEXTAUTH_URL
+    identityDbUrl: process.env.IDENTITY_DB_URL
 }
